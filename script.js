85abbd6813d92c9d85add6ea1aec177c16291a95
CoCo200301 <141877949+CoCo200301@users.noreply.github.com>
2023-09-22 13:50:07 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Función para mostrar el letrero
+function mostrarLetrero(letreroId) {
+    var letrero = document.getElementById(letreroId);
+    letrero.style.display = "block";
+}
+
+// Función para cerrar el letrero
+function cerrarLetrero(letreroId) {
+    var letrero = document.getElementById(letreroId);
+    letrero.style.display = "none";
+}
+
+// Funciones adicionales para el nuevo letrero
+function mostrarLetrero2(letreroId) {
+    var letrero = document.getElementById(letreroId);
+    letrero.style.display = "block";
+}
+
+function cerrarLetrero2(letreroId) {
+    var letrero = document.getElementById(letreroId);
+    letrero.style.display = "none";
+}
